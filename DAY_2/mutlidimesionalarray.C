c520be7c343d701bd677ccc9334cdea9b28e3d64
solaikannan pandiyan <solaikannanpandiyan@gmail.com>
2025-01-23 15:05:07 +0530

    pic

---
+++ DAY_2/mutlidimesionalarray.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdio.h>
+
+// command to run: gcc pointersAdvanced.C -o advanceexe && ./advanceexe
+
+int main() {
+    patternno = 5;
+    for(int i = 1; i <= 5; i ++){
+        for(int j = 1; j <= 5; j ++){
+        
+        }
+        printf("\n");
+    }
+
+
+}
