91434a4cd9c327d96b73bdc4a8c2f4d8a567afa0
solaikannan pandiyan <solaikannanpandiyan@gmail.com>
2025-01-28 15:01:25 +0530

    new change

--- DAY_5/calculator.py
+++ DAY_5/calculator.py
@@ -1,17 +1,16 @@
 
 
-def calculator(x, y):
-    return x+y
+# def calculator(x, y):
+#     return x+y
 
-opp = "mul"
+# opp = "mul"
 
-# add 2, 3
-# jump 101
-if opp == "add": # true jump 1002 | false = jump 1004 #add 1000
-    print(calculator(2,3)) #add 1002
-    if True:
-        print("hi")
-else:   #add 1003
-    print("not supported") #add 1004
-    print()
-    
+# # add 2, 3
+# # jump 101
+# if opp == "add": # true jump 1002 | false = jump 1004 #add 1000
+#     print(calculator(2,3)) #add 1002
+#     if True:
+#         print("hi")
+# else:   #add 1003
+#     print("not supported") #add 1004
+#     print()
---
+++ DAY_5/controlflows.py
@@ -0,0 +1,48 @@
+
+# FOR LOOP and RANGE
+
+
+# always start from start value
+# always return till a step value smaller
+# than or greater than stop based on step value
+# the values increment or decrements by count    
+# -3 -2 -1 0 1 2 3 4
+# ------------------> number line 
+# which is greater -1 or -3 ? -1
+
+
+for i in range(1,5): # 1, 2, 3, 4
+    # print(i)
+    pass
+
+for i in range(1,5,2): # 1, 3
+    # print(i)
+    pass
+    
+for i in range(1,-4,-1): 
+# 1, 0, -1, -2, -3
+    # print(i) 
+    pass
+    
+for i in range(1,-4,-1): 
+# 1, 0, -1, -2, -3
+    # print(i) 
+    pass
+
+for i in range(1,4,-1): 
+# 1, 0, -1, -2, -3
+    print(i) 
+    
+# -32,768 to 32,767
+# 1, 0, -1, -2, ... -32,768, 32,767, 32,766, ..... 6, 5
+
+# range(1,3,1)
+# start = 1
+# stop = 3
+# step = 1
+# start < stop and step is positive
+# start > stop and step is negative
+# lastvalue = 1 < stop
+# lastvalue + step = 1 + 1 = 2 < stop
+# lastvalue + step = 2 + 1 = 3 < stop = false
+# 1 , 2
