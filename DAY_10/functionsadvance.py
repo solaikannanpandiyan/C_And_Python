345208bdcf8e29f39cd4eb15aaa3d49351aef306
solaikannan pandiyan <solaikannanpandiyan@gmail.com>
2025-02-03 12:36:24 +0530

    decorators

--- DAY_10/functionsadvance.py
+++ DAY_10/functionsadvance.py
@@ -37,11 +37,65 @@ def multiplier(x): # x value is in enclosing scope for function multiply
         return x * y # x is in enclosing scope variable
     return multiply # returning function
 
-print("multiplier : ",type(multiplier))
-double = multiplier(2) # x=2; 
-print("double : ",type(double))
-triple = multiplier(3) # x=3; 
-print("triple : ",type(triple))
+# print("multiplier : ",type(multiplier))
+# double = multiplier(2) # x=2; 
+# print("double : ",type(double))
+# triple = multiplier(3) # x=3; 
+# print("triple : ",type(triple))
 
-print(double(10)) # y=10; 20
-print(triple(10)) # y=10; 30
+# print(double(10)) # y=10; 20
+# print(triple(10)) # y=10; 30
+
+# DECORATORS -> syntax
+# closure -> input function and output is also function
+# log all input to the funciton
+# also log all output from the function
+
+
+# without decorator approach
+# tuple variable arguments
+# def summ(*x):
+#     print("input : ",x) # not business function
+#     output = sum(x)
+#     print("output : ", output) # not business function
+#     return output
+
+# def productt(*x):
+#     print("input : ",x) # not business function
+#     prod = 1
+#     for i in x:
+#         prod *= i
+#     print("output : ", prod) # not business function
+#     return prod
+
+# print(summ(1,2,3,4,5,6))
+# print(productt(1,2,3,4,5,6))
+
+
+def funclogger(func): # input function | func -> enclosing scope
+    
+    def wrapped(*args):
+        print("input : ",args)
+        output = func(*args)
+        print("output : ", output)
+        return output
+    
+    return wrapped # output function
+
+def summ(*x):
+    return sum(x)
+
+def productt(*x):
+    prod = 1
+    for i in x:
+        prod *= i
+    return prod
+
+# loggedsum = funclogger(summ) # summ function is wrapped
+# loggedproduct = funclogger(productt) # productt function is wrapped
+print(summ(1,2,3,4,5,6))
+print(productt(1,2,3,4,5,6))
+
+
+    
+
